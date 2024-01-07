70c093e8e906da76e882dff8984892c3acc39a8e
MertSAP <151512261+MertSAP@users.noreply.github.com>
2024-01-07 14:34:49 +0800

    Fixed bug in example

--- srv/handlers/ConditionalMandatoryCheck.js
+++ srv/handlers/ConditionalMandatoryCheck.js
@@ -1,10 +1,9 @@
 const BaseValidation = require('capval')
 module.exports = class ConditionalMandatoryCheck extends BaseValidation {
     isValid(InputValue) {
-       // console.log(this.getNode())
        var data = this.getDataToRoot();
 
-       if(data['Travel.TravelStatus_code'] === 'X') return false;
+       if(data['Travel-TravelStatus_code'] === 'X') return true;
         if(!InputValue) {
             this.setMessage("i18n>" + this.getField()+ '-errorMessage'); 
            
